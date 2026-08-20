fe3917305b51d5f9e6b9bde8085845b7dc0dcfc6
NatanelDev999 <natanael.reis1606+blion@aluno.educamericana.sp.gov.br>
2026-08-20 15:13:20 -0300

    Criei a LibraryVectorNatan

---
+++ Projetosproprios/LibraryVectorNatan.py
@@ -0,0 +1,51 @@
+#---------------------------------------------------------------------
+#                         LIBRARYVECTORNATAN
+#---------------------------------------------------------------------
+#                            BIBLIOTECAS
+import math
+#---------------------------------------------------------------------
+#biblioteca para manipulação de vetores
+#---------------------------------------------------------------------
+#                        OPERAÇÕES MATEMÁTICAS
+#---------------------------------------------------------------------
+# soma de vetores
+def soma_vetores(vetor1,vetor2):
+    vetor_saida = []
+    for pos0,i in enumerate(vetor1):
+        vetor_saida.append(vetor2[pos0]+i)
+    return vetor_saida
+# subtração de vetores
+def subtrai_vetores(vetor1,vetor2):
+    vetor_saida = []
+    for pos0,i in enumerate(vetor1):
+        vetor_saida.append(i-vetor2[pos0])
+    return vetor_saida
+# multiplicação de vetores
+def multiplica_vetores(vetor1,vetor2):
+    vetor_saida = []
+    for pos0, i in enumerate(vetor1):
+        vetor_saida.append(i * vetor2[pos0])
+    return vetor_saida
+# divisão de vetores
+def divide_vetores(vetor1,vetor2):
+    vetor_saida = []
+    for pos0, i in enumerate(vetor1):
+        vetor_saida.append(i / vetor2[pos0])
+    return vetor_saida
+#---------------------------------------------------------------------
+# função para pegar o tamanho do vetor
+def tamanho_vetor(vetor):
+    tamanho = 0
+    for i in vetor:
+        tamanho += i**2
+    return abs(math.sqrt(tamanho))
+# função para normalizar um vetor
+def normaliza_vetor(vetor):
+    vetor_saida = []
+    divisor = 0
+    for i in vetor:
+        divisor += i**2
+    divisor = math.sqrt(divisor)
+    for v in vetor:
+        vetor_saida.append(v/divisor)
+    return vetor_saida
--- Projetosproprios/desafioTela1D.py
+++ Projetosproprios/desafioTela1D.py
@@ -4,15 +4,15 @@ tela = []
 def cria_tela(altura, largura):
     global tela
     tamanho = altura * largura
-    tamanho = tamanho - (tamanho % 10)
+    tamanho = tamanho - (tamanho % largura)
     for c in range(0, tamanho):
         tela.append(0)
 
 
-def desenha_tela():
+def desenha_tela(largura):
     global tela
     for pos0, c in enumerate(tela):
-        if pos0 % 10 != 0:
+        if pos0 % largura != 0:
             print(c, end=' ')
         else:
             print()
@@ -30,7 +30,7 @@ def desenha_ponto(xy, altura, largura):
     for pos0, c in enumerate(tela):
         if cY == xy[1] and cX == xy[0]:
             tela[pos0] = 1
-        if pos0 % 10 == 0:
+        if pos0 % largura == 0:
             cY += 1
             cX = 0
         else:
@@ -41,8 +41,8 @@ def main():
     altura = 8
     largura = 9
     cria_tela(altura, largura)
-    desenha_ponto([0, 0], altura, largura)
-    desenha_tela()
+    desenha_ponto([-4, 0], altura, largura)
+    desenha_tela(largura)
 
 
 main()
