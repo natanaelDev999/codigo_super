1023ae804b59cba1ec0d1191f005991be3dfb6a9
natanaelDev999 <natanael.reis1606@aluno.educamericana.sp.gov.b>
2026-08-20 15:27:51 -0300

    Melhorei a visualização do teste da LVN

--- Projetosproprios/testeLibraryVectorNatan.py
+++ Projetosproprios/testeLibraryVectorNatan.py
@@ -3,16 +3,11 @@ import LibraryVectorNatan
 vetor = (2,1)
 vetor2 = (1,0)
 
-tamanho = LibraryVectorNatan.tamanho_vetor(vetor)# 0.000015
+tamanho = LibraryVectorNatan.tamanho_vetor(vetor)
 vetor_normalizado = LibraryVectorNatan.normaliza_vetor(vetor)
 soma = LibraryVectorNatan.soma_vetores(vetor,vetor2)
 subtracao = LibraryVectorNatan.subtrai_vetores(vetor,vetor2)
 produto = LibraryVectorNatan.multiplica_vetores(vetor,vetor2)
 quociente = LibraryVectorNatan.divide_vetores(vetor2,vetor)
-print(soma)
-print(subtracao)
-print(produto)
-print(quociente)
-print(tamanho)
-print(vetor_normalizado)
+print(f"soma: {soma}; subtracao: {subtracao};produto: {produto}; quociente: {quociente}; tamanho: {tamanho}; normalização: {vetor_normalizado}")
 
