7e0c3a0040a9633ba2bbd2150e93c69646b1f3bc
natanaelDev999 <natanael.reis1606@aluno.educamericana.sp.gov.b>
2026-08-28 19:12:24 -0300

    Melhorei o Ray Tracing 2

--- Projetosproprios/RayTracing2.py
+++ Projetosproprios/RayTracing2.py
@@ -1,7 +1,7 @@
 import LibraryVectorNatan as lvn
 #vetores
 vetor_pos = [0,0,0]
-vetor_dir = [-2,0,1]
+vetor_dir = [0,0,1]
 #raytracing variáveis
 y_visao = 4
 x_visao = 4
@@ -9,14 +9,14 @@ x_visao = 4
 matriz_mun = [
     [
         [0,0,0,0],
-        [0,1,0,0],
+        [0,0,0,0],
         [0,0,0,0],
         [0,0,0,0]
     ],
     [
         [0,0,0,0],
-        [0,1,0,0],
-        [0,0,1,0],
+        [1,1,1,1],
+        [1,1,1,1],
         [0,0,0,0]
     ],
     [
@@ -33,19 +33,19 @@ matriz_mun = [
     ]
 ]
 #visão
-matriz_visual = [[' ',' ',' ',' '],
-        [' ',' ',' ',' '],
-        [' ',' ',' ',' '],
-        [' ',' ',' ',' ']]
+matriz_visual = [['.','.','.','.'],
+        ['.','.','.','.'],
+        ['.','.','.','.'],
+        ['.','.','.','.']]
 def raytracing():
     global vetor_pos,vetor_dir,x_visao,y_visao
     for y in range(0,y_visao):
         for x in range(0,x_visao):
             vetor_ini = [x+vetor_pos[0],y+vetor_pos[1],vetor_pos[2]]
             while True:
-                if vetor_ini[2] < 4 and vetor_ini[1] < 4 and vetor_ini[0] < 4 and vetor_ini[2] > 0 and vetor_ini[1] > 0 and vetor_ini[0] > 0:
-                    if matriz_mun[vetor_ini[2]][vetor_ini[1]][vetor_ini[0]] == 1:
-                        matriz_visual[vetor_ini[1]][vetor_ini[0]] = '#'
+                if round(vetor_ini[2]) < 4 and round(vetor_ini[1]) < 4 and round(vetor_ini[0]) < 4 and round(vetor_ini[2]) >= 0 and round(vetor_ini[1]) >= 0 and round(vetor_ini[0]) >= 0:
+                    if matriz_mun[round(vetor_ini[2])][round(vetor_ini[1])][round(vetor_ini[0])] == 1:
+                        matriz_visual[round(vetor_ini[1])][round(vetor_ini[0])] = '█'
                         break
                     else:
                         vetor_ini = lvn.soma_vetores(vetor_ini,vetor_dir)
@@ -60,7 +60,7 @@ def desenha_tela():
     global matriz_visual
     for c in matriz_visual:
         for v in c:
-            print('\033[0;0;43m',v,'\033[m',end='')
+            print(v,end=' ')
         print()
 
 def main():
