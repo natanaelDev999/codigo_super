d156941d0575ba4f53613e2196778523727e3594
natanaelDev999 <natanael.reis1606@aluno.educamericana.sp.gov.b>
2026-09-03 18:53:57 -0300

    Atualizações no WikiNatan; WikiNatan 1.2

--- Projetosproprios/WikiNatan/cliente_WikiNatan.py
+++ Projetosproprios/WikiNatan/cliente_WikiNatan.py
@@ -6,17 +6,17 @@ nome = ""
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((p,port))
-nome = str(input("Insira um nome como utilizador: "))
+nome = str(input("\033[33mInsira um nome como utilizador:\033[33m "))
 while True:
-    mensagem = input('Deseja criar um artigo?: ').upper()
+    mensagem = input('Deseja criar um \033[34martigo\033[m?: ').upper()
     if mensagem == "S":
-        titulo = str(input("Insira o título do artigo: "))
-        artigo = str(input("Insira o texto do artigo: "))
+        titulo = str(input("\033[34mInsira o título do artigo\033[m: "))
+        artigo = str(input("\033[34mInsira o texto do artigo\033[m: "))
         socket.send((f"={nome}/{titulo}/{artigo}").encode())
-        print('artigo criado e mandado ao servidor com sucesso')
-    mensagem = str(input("Deseja ler um artigo?: ")).upper()
+        print('\033[34martigo criado e mandado ao servidor com sucesso\033[m')
+    mensagem = str(input("\033[35mDeseja ler um artigo?\033[m: ")).upper()
     if mensagem == "S":
-        titulo = str(input("Insira o título do artigo: "))
+        titulo = str(input("\033[35mInsira o título do artigo\033[m: "))
         socket.send((f"-{titulo}").encode())
         artigo = socket.recv(1024).decode()
         print(artigo)
