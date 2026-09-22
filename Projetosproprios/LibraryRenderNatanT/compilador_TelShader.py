93c60235563090341654453f88e3b1fa7b4ea2e9
natanaelDev999 <natanael.reis1606@aluno.educamericana.sp.gov.b>
2026-09-22 19:06:36 -0300

    Intrigado com simulações criei um teste de gravidade com uma partícula

--- Projetosproprios/LibraryRenderNatanT/compilador_TelShader.py
+++ Projetosproprios/LibraryRenderNatanT/compilador_TelShader.py
@@ -2,9 +2,8 @@
 def compila_codigo_TelShader(codigo_TelShader,pixel,x,y):
     pixel_saida = ' '
     linha = ''
-    x1 = x
-    y1 = y
     variaveis = []
+    vetor = []
     for c in codigo_TelShader:
         if c != ';':
             linha += c
@@ -18,59 +17,90 @@ def compila_codigo_TelShader(codigo_TelShader,pixel,x,y):
                     pixel_saida = chr
             elif linha.startswith('cp=') or linha.startswith('cp ='):
                 vars,chr = linha.split('=')
-                r,g,b = chr.split(',')
-                pixel_saida = f'\033[38;2;{r};{g};{b}m{pixel_saida}\033[m'
+                if ',' in chr:
+                    r,g,b = chr.split(',')
+                    pixel_saida = f'\033[38;2;{r};{g};{b}m{pixel_saida}\033[m'
+                else:
+                    for v in vetor:
+                        if v[0] == chr:
+                            if v[2] == 'vec3':
+                                r = v[1]["0"]
+                                g = v[1]["1"]
+                                b = v[1]["2"]
+                                pixel_saida = f'\033[38;2;{int(r)};{int(g)};{int(b)}m{pixel_saida}\033[m'
             elif linha.startswith('x=') or linha.startswith('x ='):
                 vars,pos = linha.split('=')
                 if pos.isnumeric():
-                    x1 = int(pos)
+                    x = int(pos)
                 else:
                     for i in variaveis:
                         if i[0] == pos:
-                            x1 = i[1]
+                            x = i[1]
                             break
             elif linha.startswith('y=') or linha.startswith('y ='):
                 vars,pos = linha.split('=')
                 if pos.isnumeric():
-                    y1 = int(pos)
+                    y = int(pos)
                 else:
                     for i in variaveis:
                         if i[0] == pos:
-                            y1 = i[1]
+                            y = i[1]
             elif linha.startswith('v'):
                 operacao, valor1, valor2 = linha.split(' ')
                 if valor1 == 'x':
                     if valor2.isnumeric():
-                        x1 += float(valor2)
+                        x += float(valor2)
                     else:
                         for i in variaveis:
                             if i[0] == valor2:
-                                x1 += i[1]
+                                x += i[1]
+                                break
                 elif valor1 == 'y':
                     if valor2.isnumeric():
-                        y1 += float(valor2)
+                        y += float(valor2)
                     else:
                         for i in variaveis:
                             if i[0] == valor2:
-                                y1 += i[1]
+                                y += i[1]
+                                break
             elif linha.startswith('s'):
                 operacao, valor1, valor2 = linha.split(' ')
                 if valor1 == 'x':
                     if valor2.isnumeric():
-                        x1 -= float(valor2)
+                        x -= int(valor2)
                     else:
                         for i in variaveis:
                             if i[0] == valor2:
-                                x1 -= i[1]
+                                x -= i[1]
+                                break
                 elif valor1 == 'y':
                     if valor2.isnumeric():
-                        y1 -= float(valor2)
+                        y -= int(valor2)
                     else:
                         for i in variaveis:
                             if i[0] == valor2:
-                                y1 -= i[1]
+                                y -= i[1]
+                                break
             elif linha.startswith('ptf'):
                 comando, nome , valor = linha.split(' ')
-                variaveis.append([nome,float(valor)])
+                if valor.isnumeric():
+                    variaveis.append([nome,float(valor)])
+                else:
+                    for i in variaveis:
+                        if i[0] == valor:
+                            variaveis.append([nome,i[1]])
+                            break
+            elif linha.startswith('2ptf'):
+                comando, nome = linha.split(' ')
+                vetor.append([nome,{"0":0,"1":0},"vec2"])
+            elif linha.startswith('3ptf'):
+                comando, nome = linha.split(' ')
+                vetor.append([nome,{"0":0,"1":0,"2":0},"vec3"])
+            elif linha.startswith('mdf'):
+                comando, nome , pos , valor = linha.split(' ')
+                for v in vetor:
+                    if v[0] == nome:
+                        v[1][f"{pos}"] = float(valor)
+                        break
             linha = ''
-    return [pixel_saida,int(x1),int(y1)]
+    return [pixel_saida,int(x),int(y)]
